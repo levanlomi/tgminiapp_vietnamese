828c76f287ef755484af55ecaecedd91fb1ec563
levanlomi <lomidze92@gmail.com>
2026-07-19 09:54:03 +0700

    upd app version - v19

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tieng-viet-v18'; // <--- Изменяем версию кэша каждый раз при обновлении кода
+const CACHE_NAME = 'tieng-viet-v19'; // <--- Изменяем версию кэша каждый раз при обновлении кода
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
