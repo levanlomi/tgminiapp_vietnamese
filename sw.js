9a59a18642ea442a9e54181e8d7af75ea7cf61d4
levanlomi <lomidze92@gmail.com>
2026-07-28 13:58:36 +0700

    Новые слова+поиск по кнопке+убрал дубли карточек+оставил 1 иконку стола (кастомную)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tieng-viet-v22'; // <--- Изменяем версию кэша каждый раз при обновлении кода
+const CACHE_NAME = 'tieng-viet-v23'; // <--- Изменяем версию кэша каждый раз при обновлении кода
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
